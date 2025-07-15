f88f4574ba77cc3cecfe73ad7b167286265c335c
Dmitrii <miliukov.dmitrii@icloud.com>
2025-07-15 13:15:54 +0400

    Initial Commit

---
+++ GoTravel-iOS/GoTravel_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTravel_iOSApp.swift
+//  GoTravel-iOS
+//
+//  Created by Dmitrii on 15.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct GoTravel_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
